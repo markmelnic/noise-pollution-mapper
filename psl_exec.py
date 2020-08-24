0349f0eb7ee6f7f910254959a78a0203cd45a297
Mark Melnic <markhappy200@gmail.com>
2020-08-24 22:27:48 +0200

    Access point improvements

--- psl_exec.py
+++ psl_exec.py
@@ -2,7 +2,7 @@
 import sys
 import subprocess
 
-def exec_gps():
+def exec_gps() -> list:
     cmd = ["PowerShell", "-ExecutionPolicy", "Unrestricted", "-File", ".\\pshell.ps1"]
     ec = str(subprocess.check_output(cmd, shell=True))
     return ec[80:-13].split(" ")
--- test_access_point.py
+++ test_access_point.py
@@ -1,33 +1,32 @@
 
 import googlemaps
+from key import KEY2
 
+def access_point():
 
-with open("key.txt", mode='r') as keyfile:
-    key = keyfile.read().splitlines()
-    keyfile.close()
-    
-gmaps = googlemaps.Client(key = key[1])
-    
-celltower = {
-    "cellId": 42345,
-    "locationAreaCode": 4002,
-    "mobileCountryCode": 259,
-    "mobileNetworkCode": 2
-}
+    gmaps = googlemaps.Client(key = KEY2)
 
-wifiaccess = {
-    
-}
+    # sample cell tower
+    celltower = {
+        "cellId": 42345,
+        "locationAreaCode": 4002,
+        "mobileCountryCode": 259,
+        "mobileNetworkCode": 2
+    }
 
-try:
-    loc = gmaps.geolocate('259', 
-                          '2', 
-                          'lte', 
-                          'Moldcell', 
-                          True, 
-                          list(celltower)
-                          )
+    # sample wifi access point
+    wifiaccess = {
+    }
 
-    print(loc)
-except Exception as e:
-    print(e.message)
+    try:
+        loc = gmaps.geolocate('259', 
+                            '2', 
+                            'lte', 
+                            'Moldcell', 
+                            True, 
+                            list(celltower)
+                            )
+
+        print(loc)
+    except Exception as e:
+        print(e.message)
